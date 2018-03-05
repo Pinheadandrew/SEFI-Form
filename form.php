a86916e87596982785bf65566e06d019c7adcd7e
ThePinhead <sigalasa1@admsu.montclair.edu>
2018-03-05 14:21:06 -0500

    3/5
    Website to submit SEFI Requests for Services electronically, and for SEFI staff to review them.

---
+++ form.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>SEFI Form</title>
+        <link type="text/css" rel="stylesheet" href="formstyle.css">
+        <style>
+        </style>
+    </head>
+    <body>
+<!--
+    <table>
+        <?php
+        for($i = 0; $i < 224; $i++)
+        {
+        ?>
+        <tr>
+            <td>No</td>
+            <td>No</td>
+            <td>No</td>
+            <td>No</td>
+            <td>No</td>
+            <td>No</td>
+            <td>No</td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+-->
+        
+    <form>
+        <label>First Name: <input type="text" id="firstname" /></label><br>
+        <input type="text" id="lastname" /><br>
+        <input type="text" id="middlename" /><br>
+        <input type="date" id="ph-number" /><br>
+        <input type="password" id="password" /><br>
+        <input type="text" /><br>
+        <input type="text" /><br>
+        
+    </form>
+    </body>
+
+
+</html>

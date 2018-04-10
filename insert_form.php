ebbc1f01549d5099c812cb5af92ce9dd314d381a
ThePinhead <sigalasa1@admsu.montclair.edu>
2018-04-10 15:00:36 -0400

    Store files to directory based on input element they're submitted through.

--- insert_form.php
+++ insert_form.php
@@ -3,14 +3,15 @@
 
 	foreach($_POST as $inputfield => $value)
 	{
-		echo $inputfield.": ".$value;
-		
-		if (strpos($inputfield, 'upload') !== false) 
-		{
-    	echo ' (This is also a file upload field)';
-		}
-		echo "<br>";
+		echo "$inputfield: $value<br>";
 	}
-	
-	include "functions/save_upload.php";
+		
+// Prints file field names and the name of the submitted files.
+	echo '<strong>Files Uploads:</strong><br>';
+	foreach($_FILES as $file_field => $file)
+		{
+			$f_name = $file['name'];
+			echo "<strong>".$file_field.": ".$f_name."</strong><br>";
+			include "functions/save_upload.php";
+		}
 ?>

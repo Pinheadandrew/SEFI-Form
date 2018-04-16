be63920c81b5f2c36d65aa50bc0cd2dad89606f4
ThePinhead <sigalasa1@admsu.montclair.edu>
2018-04-16 13:55:44 -0400

    Changed value attributes due to change in database.
    Form insertion script can now add new entries to database. Started to address cases of missing fields by returning to form page.

--- insert_form.php
+++ insert_form.php
@@ -3,12 +3,17 @@
 
 	$insertValues = array();
 		
-	// Prints name of each input element and what has been submitted. Stores them in dictionary $queryArguments 
-	// so inputs later used in query string.
+	// Stores name of each input element and what has been submitted in dictionary $queryArguments so inputs later used in query string.
 	foreach($_POST as $inputfield => $value)
 	{
-		// echo "$inputfield: $value<br>";
-		$insertValues[$inputfield] = $value;
+		//  If a field is empty, send them back to form page with message that their was a missing field in their submission.";
+		if (empty($_POST[$inputfield]))
+		{
+			header('Location: form.html');
+			echo "NOT FILLED!<br>";
+		}
+		
+		$insertValues[$inputfield] = htmlspecialchars($value);
 		echo "$inputfield: ".$insertValues[$inputfield]."<br>";
 	}
 		
@@ -23,7 +28,7 @@
 
 	// Sets size of class variable to be the value in input field corresponding to the class level(pre-k, toddlers).
 	// i.e. values are 10 for pre-k, 5 for toddlers. If pre-k is selected radio button, then class size variable set to 10.
-	if ($insertValues['classlevel'] == "preschool")
+	if ($insertValues['classlevel'] == "Preschool")
 	{
 		$classSize = $insertValues['classsize_prek'];
 	}
@@ -33,7 +38,7 @@
 	}
 	// Build the insert statement that will append to the database the new form's data.
 	require "dbconnect.php";
-	$insert_query = "insert into form values('',
+	$insert_query = "insert into form values(null,
 			'".$insertValues['org-name']."',
 			'".$insertValues['org-address']."',
 			'".$insertValues['org-phnumber']."',
@@ -45,17 +50,22 @@
 			'".$insertValues['tasemail']."',
 			'".$insertValues['gnjktransdate']."',
 			'".$insertValues['gnjkregion']."',
-			'".$insertValues['classlevel']."',
-			'".$classSize."',
 			'".$insertValues['trainingtype']."',
 			'".$insertValues['duration']."',
 			'".$insertValues['timeofday']."',
 			'".$insertValues['dayofweek']."',
+			'".$insertValues['classlevel']."',
+			'".$classSize."',
 			'','')";
-	echo $insert_query;
+	echo $insert_query."<br>";
 	$db->query($insert_query);
 	if($db->error)
 	{
-			echo "What.<br>";
+			echo "There was an error submitting your form.<br>";
 	}
+	else
+	{
+			echo "Your form was submitted succesfully. Expect a period of up to two weeks to hear back on your request's approval.<br>";
+	}
+$db->close();
 ?>
